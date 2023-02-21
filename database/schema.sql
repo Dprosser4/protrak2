dd3e09592a0cc7517bb05015308314f83182d3be
Dylan Prosser <dprosser4@gmail.com>
2023-02-21 22:12:50 +0000

    initial project setup with database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,52 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+ CREATE TABLE "public"."users" (
+	"userId" serial,
+	"username" TEXT NOT NULL UNIQUE,
+	"hashedPassword" TEXT NOT NULL,
+	"createAt" timestamptz(6) not null default now() ,
+	"role" TEXT,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."projects" (
+	"projectId" serial NOT NULL,
+	"poNumber" TEXT NOT NULL UNIQUE,
+	"name" TEXT NOT NULL,
+	"address" TEXT NOT NULL,
+	"city" TEXT NOT NULL,
+	"state" TEXT NOT NULL,
+	"zipcode" TEXT NOT NULL,
+	"notes" TEXT NOT NULL,
+	"completed" BOOLEAN NOT NULL,
+	"assignedTo" serial NOT NULL,
+	"createdAt" timestamptz NOT NULL DEFAULT 'now()',
+	CONSTRAINT "projects_pk" PRIMARY KEY ("projectId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."image" (
+	"imageId" serial NOT NULL,
+	"url" TEXT NOT NULL,
+	"project" serial NOT NULL,
+	CONSTRAINT "image_pk" PRIMARY KEY ("imageId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "projects" ADD CONSTRAINT "projects_fk0" FOREIGN KEY ("assignedTo") REFERENCES "users"("userId");
+
+ALTER TABLE "image" ADD CONSTRAINT "image_fk0" FOREIGN KEY ("project") REFERENCES "projects"("projectId");

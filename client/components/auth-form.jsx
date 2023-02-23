a2220daa395eb45cff7f9af89a9fadad4f2d507f
Dylan Prosser <dprosser4@gmail.com>
2023-02-23 17:54:39 +0000

    setup basic app structure with login and log out

---
+++ client/components/auth-form.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function AuthForm({ action, onSignIn }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const req = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    };
+    fetch(`/api/auth/${action}`, req)
+      .then(res => res.json())
+      .then(result => {
+        if (action === 'sign-up') {
+          window.location.hash = 'sign-in';
+        } else if (result.user && result.token) {
+          onSignIn(result);
+        }
+      });
+  }
+
+  const submitButtonText = action === 'sign-up'
+    ? 'Register'
+    : 'Log In';
+
+  return (
+    <div>
+      <div className="">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Userame:</Form.Label>
+            <Form.Control
+                type="text"
+                placeholder="Enter username"
+              onChange={e => setUsername(e.target.value)}
+                required
+              />
+          </Form.Group>
+
+          <Form.Group className='mb-3' controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+              onChange={e => setPassword(e.target.value)}
+                required
+              />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {submitButtonText}
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import LoginPage from '../components/hello-world';
+import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import AppContext from '../lib/app-context';
+import Redirect from '../components/redirect';
 
-export default function Home(props) {
+export default function Home() {
+  const { user, handleSignOut } = useContext(AppContext);
+  if (!user) return <Redirect to="sign-in" />;
+
   return (
     <div>
-      <LoginPage />
+      <h1>Hey You Logged in!</h1>
+      <Button variant="primary" onClick={handleSignOut}>
+        Sign Out
+      </Button>
     </div>
   );
 }

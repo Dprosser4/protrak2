fbc20f78494d69bf55603c047d760c661f9c1932
Dylan Prosser <dprosser4@gmail.com>
2023-03-08 00:56:25 +0000

    changed dummy login data

--- database/data.sql
+++ database/data.sql
@@ -1,14 +1,6 @@
 insert into "users" ("username", "hashedPassword", "role", "firstName" , "lastName")
-values ('dylan1', '$argon2id$v=19$m=4096,t=3,p=1$PnqCcNmXM6LKe+e8i9E8Zw$mzcDy8WZvSWpZRWsRGaSump9SCuKntniWL6vVCHPHJ4', 'tech', 'Dylan', 'Prosser');
+values ('dylan1', '$argon2id$v=19$m=4096,t=3,p=1$PnqCcNmXM6LKe+e8i9E8Zw$mzcDy8WZvSWpZRWsRGaSump9SCuKntniWL6vVCHPHJ4', 'admin', 'Dylan', 'Prosser');
 
-insert into "users" ("username", "hashedPassword", "role", "firstName" , "lastName")
-values ('chase1', '$argon2id$v=19$m=4096,t=3,p=1$PnqCcNmXM6LKe+e8i9E8Zw$mzcDy8WZvSWpZRWsRGaSump9SCuKntniWL6vVCHPHJ4', 'admin', 'Chase', 'Prosser');
-
-insert into "users" ("username", "hashedPassword", "role", "firstName" , "lastName")
-values ('ryan1', '$argon2id$v=19$m=4096,t=3,p=1$PnqCcNmXM6LKe+e8i9E8Zw$mzcDy8WZvSWpZRWsRGaSump9SCuKntniWL6vVCHPHJ4', 'tech', 'Ryan', 'Prosser');
-
-insert into "users" ("username", "hashedPassword", "role", "firstName" , "lastName")
-values ('ron1', '$argon2id$v=19$m=4096,t=3,p=1$PnqCcNmXM6LKe+e8i9E8Zw$mzcDy8WZvSWpZRWsRGaSump9SCuKntniWL6vVCHPHJ4', 'tech', 'Ron', 'Prosser');
 
 
 insert into "projects" ("poNumber","name", "address", "city", "state", "zipcode")

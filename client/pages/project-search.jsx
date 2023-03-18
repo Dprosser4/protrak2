26add8ca40d7df18e68682859837826533d94e67
Dylan Prosser <dprosser4@gmail.com>
2023-03-18 00:08:23 +0000

    added search by tech and po

--- client/pages/project-search.jsx
+++ client/pages/project-search.jsx
@@ -4,13 +4,16 @@ import Redirect from '../components/redirect';
 import UpdateForm from '../components/update-form';
 import ProjectsAccordian from '../components/project-accordian';
 import { ScaleLoader } from 'react-spinners';
-import { Button } from 'react-bootstrap';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 
 export default function ProjectSearch() {
   const [projects, setProjects] = useState([]);
   const [updateProject, setUpdateProject] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [queryProjects, setQueryProjects] = useState([]);
+  const [poNumber, setPoNumber] = useState('');
+  const [assignedTo, setAssignedTo] = useState('');
+  const [queryMessage, setQueryMessage] = useState('Search For Projects');
 
   function handleUpdateClick(project) {
     setUpdateProject(project);
@@ -22,10 +25,6 @@ export default function ProjectSearch() {
 
   function onCancel() {
     setUpdateProject(null);
-  }
-
-  function handleSearch() {
-    setQueryProjects(completedProjects);
   }
 
   useEffect(() => {
@@ -55,16 +54,64 @@ export default function ProjectSearch() {
   const completedProjects = projects.filter((project) => project.completed);
   const unassignedProjects = projects.filter((project) => project.assignedTo === null && !project.completed);
   const assignedProjects = projects.filter((project) => project.assignedTo && !project.completed);
+  const { techs } = useContext(AppContext);
+
+  function handleSearch() {
+    setQueryProjects(completedProjects);
+  }
+
+  function searchByPoNumber() {
+    setQueryProjects(projects.filter((project) => project.poNumber === poNumber));
+  }
+
+  function searchByTechnician() {
+    setQueryProjects(projects.filter((project) => String(project.assignedTo) === assignedTo));
+    if (queryProjects.length === 0) {
+      setQueryMessage('No Projects Assigned To That Technician');
+    }
+  }
 
   return (
     <div className="col-lg-4 text-align-center">
+      <Form onSubmit={searchByPoNumber}>
+        <Form.Label>Search By PO Number:</Form.Label>
+        <InputGroup className='mb-3' controlId="formPoNumber">
+          <Form.Control
+            type="text"
+            placeholder="Enter PO Number e.g. PT12345"
+            value={poNumber}
+            onChange={(e) => setPoNumber(e.target.value)}
+            required
+          />
+          <Button type='submit' variant="outline-secondary" id="button-addon2">
+            Search
+          </Button>
+        </InputGroup>
+      </Form>
+
+      <Form onSubmit={searchByTechnician}>
+        <Form.Label>Search By Technician:</Form.Label>
+        <InputGroup className='mb-3' controlId="formPoNumber">
+          <Form.Select value={assignedTo}
+            onChange={(e) => setAssignedTo(e.target.value)} >
+
+            <option value="unassigned">Choose a Technician</option>
+            {techs.map((tech) => (<option key={tech.userId} value={tech.userId} >{`${tech.firstName} ${tech.lastName}`}</option>))}
+
+          </Form.Select>
+          <Button type='submit' variant="outline-secondary" id="button-addon2">
+            Search
+          </Button>
+        </InputGroup>
+      </Form>
+
       <Button onClick={handleSearch}>Completed</Button>
       <h2 className="text-center mt-3 fs-4">
         Projects
       </h2>
       <ProjectsAccordian projects={queryProjects} handleUpdateClick={handleUpdateClick} />
       {queryProjects.length === 0 &&
-      <p className='text-center'>There are no unassigned projects.</p>
+      <p className='text-center'> {queryMessage}</p>
         }
     </div>
   );

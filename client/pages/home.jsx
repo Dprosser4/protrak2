b0e6676b33bb56b5a21140656f972abcfb0d16f8
Dylan Prosser <dprosser4@gmail.com>
2023-03-21 21:39:01 +0000

    added search by status and cleaned up responsive styling

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -9,11 +9,11 @@ export default function Home() {
 
   return (
 
-    <Col className="text-center">
+    <Col className="col-lg-4 text-center">
       <h2 className="fs-4">
         Welcome {`${user.firstName}!`}
       </h2>
-      <p className="text-muted mb-4">Protrak2 is a project mangement app for creating, updating and completion tracking of projects. Admin Users can create and update projects, assign projects to the technicians, and view when those projects are completed. Technicians can only see projects assigned to them, update notes, and mark a project as complete. Use the Navigation Bar above to try it out!</p>
+      <p className="text-muted mb-4">Protrak2 is a project mangement app for creating, updating and completion tracking of projects. Admin Users can create and update projects, assign projects to the technicians, and view when those projects are completed. Technicians can only see projects assigned to them, update notes, add images and mark a project as complete. Use the Navigation Bar above to try it out!</p>
     </Col>
   );
 }

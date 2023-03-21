0f4afc2fa3b4db43c1a9aea570ce37e79fdab4a8
Dylan Prosser <dprosser4@gmail.com>
2023-03-21 21:39:01 +0000

    added search by status and cleaned up responsive styling

--- client/pages/project-search.jsx
+++ client/pages/project-search.jsx
@@ -13,7 +13,9 @@ export default function ProjectSearch() {
   const [queryProjects, setQueryProjects] = useState([]);
   const [poNumber, setPoNumber] = useState('');
   const [assignedTo, setAssignedTo] = useState('');
-  const [queryMessage, setQueryMessage] = useState('Search For Projects');
+  const [queryMessage, setQueryMessage] = useState('Search for projects with the tools above.');
+  const [projectStatus, setProjectStatus] = useState([]);
+  const { techs } = useContext(AppContext);
 
   function handleUpdateClick(project) {
     setUpdateProject(project);
@@ -54,20 +56,36 @@ export default function ProjectSearch() {
   const completedProjects = projects.filter((project) => project.completed);
   const unassignedProjects = projects.filter((project) => project.assignedTo === null && !project.completed);
   const assignedProjects = projects.filter((project) => project.assignedTo && !project.completed);
-  const { techs } = useContext(AppContext);
-
-  function handleSearch() {
-    setQueryProjects(completedProjects);
-  }
 
   function searchByPoNumber() {
     setQueryProjects(projects.filter((project) => project.poNumber === poNumber));
+    if (queryProjects.length === 0) {
+      setQueryMessage('No Project Found with that PO Number. Reminder PO Numbers are case sensetive and must be exact match.');
+    }
   }
 
   function searchByTechnician() {
     setQueryProjects(projects.filter((project) => String(project.assignedTo) === assignedTo));
     if (queryProjects.length === 0) {
       setQueryMessage('No Projects Assigned To That Technician');
+    }
+  }
+
+  function searchByStatus(e) {
+    if (projectStatus === 'allprojects') {
+      setQueryProjects(projects);
+    }
+    if (projectStatus === 'assigned') {
+      setQueryProjects(assignedProjects);
+    }
+    if (projectStatus === 'unassigned') {
+      setQueryProjects(unassignedProjects);
+    }
+    if (projectStatus === 'completed') {
+      setQueryProjects(completedProjects);
+    }
+    if (queryProjects.length === 0) {
+      setQueryMessage('No projects found with that status.');
     }
   }
 
@@ -91,7 +109,7 @@ export default function ProjectSearch() {
 
       <Form onSubmit={searchByTechnician}>
         <Form.Label>Search By Technician:</Form.Label>
-        <InputGroup className='mb-3' controlId="formPoNumber">
+        <InputGroup className='mb-3' controlId="formAssigned">
           <Form.Select value={assignedTo}
             onChange={(e) => setAssignedTo(e.target.value)} >
 
@@ -105,9 +123,26 @@ export default function ProjectSearch() {
         </InputGroup>
       </Form>
 
-      <Button onClick={handleSearch}>Completed</Button>
+      <Form onSubmit={searchByStatus}>
+        <Form.Label>Search By Project Status:</Form.Label>
+        <InputGroup className='mb-3' controlId="formStatus">
+          <Form.Select value={projectStatus}
+            onChange={(e) => setProjectStatus(e.target.value)} >
+
+            <option value='allprojects'>All Projects </option>
+            <option value='unassigned'>Unassigned Projects </option>
+            <option value='assigned'>Assigned Projects </option>
+            <option value='completed'>Completed Projects </option>
+
+          </Form.Select>
+          <Button type='submit' variant="outline-secondary" id="button-addon2">
+            Search
+          </Button>
+        </InputGroup>
+      </Form>
+
       <h2 className="text-center mt-3 fs-4">
-        Projects
+        Projects:
       </h2>
       <ProjectsAccordian projects={queryProjects} handleUpdateClick={handleUpdateClick} />
       {queryProjects.length === 0 &&
